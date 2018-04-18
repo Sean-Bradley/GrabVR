9e398b5608b3eeecfd25a2a7c5769fa4f43766b9
Sean-Bradley <sbradley@sky-futures.com>
2018-04-18 18:21:52 +0100

    updated

--- grabvr.js
+++ grabvr.js
@@ -14,81 +14,104 @@
      * https://seanwasere.com/
      */
 
-    var GrabVR = (function (scene) {
+    var GrabVR = (function (scene, usePhysijs) {
         var controllers = [];
         var _grabableDistance = 10;
         var _grabableObjects = [];
         var direction = new THREE.Vector3(0, 0, -1);
+        
+        var _usePhysijs = false;
+        if (!!usePhysijs) {
+            _usePhysijs = usePhysijs;
+            console.log("usePhysijs = " + _usePhysijs);
+        }
+        
 
         return {
-            add: function (o) {
+            add: function (o) {                
                 o.grabVR = {
                     raycaster: new THREE.Raycaster(),
                     quaternion: new THREE.Quaternion(),
-                    line: new THREE.Object3D(),
                     grabbedObject: null,
                     hasAGrabbedObject: false,
-                    grabberHookPoint: new THREE.Object3D(),
-                    releaseObject: false,
-                    //lastPosition: new THREE.Vector3(0, 0, 0),
-                    //lastQuaternion: new THREE.Quaternion(),
-                    //velocity: new THREE.Vector3(0, 0, 0),
-                    //quaternionDifference: new THREE.Vector4(0, 0, 0, 0)
+                    mesh: new THREE.Object3D(),
+                    line: new THREE.Object3D(),
+                    grabberHook: new THREE.Object3D()
                 };
-                var geometry = new THREE.Geometry();
-                geometry.vertices.push(new THREE.Vector3(0, 0, 0));
-                geometry.vertices.push(new THREE.Vector3(0, 0, -100));
-                o.grabVR.line = new THREE.Line(geometry, new THREE.LineBasicMaterial({ color: 0x888888 }));
-                o.add(o.grabVR.line);
-                controllers.push(o);
 
-                var controllerMesh = new Physijs.BoxMesh(
-                    //new THREE.CylinderGeometry(.05, 0.05, .4, 16, 1, true),
-                    new THREE.CubeGeometry(.1, .1, .5),
-                    new THREE.MeshStandardMaterial({
-                        color: 0x00ff88,
-                        wireframe: true
-                    }),
-                    1
-                );
 
-                //controllerMesh.addEventListener('collision', function (other_object, relative_velocity, relative_rotation, contact_normal) {
-                // `this` has collided with `other_object` with an impact speed of `relative_velocity` and a rotational force of `relative_rotation` and at normal `contact_normal`
-                //    console.log("abc");
-                //this.grabbedObjectHeld = true;
-                //});
+                var controllerMesh;
+                if (_usePhysijs) {
+                    controllerMesh = new Physijs.BoxMesh(
+                        //new THREE.CylinderGeometry(.05, 0.05, .4, 16, 1, true),
+                        new THREE.CubeGeometry(.1, .1, .5),
+                        new THREE.MeshStandardMaterial({
+                            color: 0x00ff88,
+                            wireframe: true
+                        }),
+                        .1
+                    );
+                } else {
+                    controllerMesh = new THREE.Mesh(
+                        //new THREE.CylinderGeometry(.05, 0.05, .4, 16, 1, true),
+                        new THREE.CubeGeometry(.1, .1, .5),
+                        new THREE.MeshStandardMaterial({
+                            color: 0x00ff88,
+                            wireframe: true
+                        })
+                    );
+                }
                 scene.add(controllerMesh);
                 o.grabVR.mesh = controllerMesh;
 
-                var grabberPlaceHolder = new THREE.Mesh(
-                    //new THREE.CubeGeometry(.1, .1, 5),
-                    new THREE.SphereBufferGeometry(.1, 5, 5),
+                var geometry = new THREE.Geometry();
+                geometry.vertices.push(new THREE.Vector3(0, 0, 0));
+                geometry.vertices.push(new THREE.Vector3(0, 0, -100));
+                var line = new THREE.Line(geometry, new THREE.LineBasicMaterial({ color: 0x8888ff }));
+                line.visible = false;
+                controllerMesh.add(line);
+                o.grabVR.line = line;
+
+                var grabberHook = new THREE.Mesh(
+                    //new THREE.CubeGeometry(.1, .1, 50),
+                    new THREE.SphereBufferGeometry(.1, 6, 6),
                     new THREE.MeshStandardMaterial({
                         color: 0x00ff00,
-                        wireframe: true
+                        wireframe: false,
+                        depthTest: false,
+                        depthWrite: false,
+                        transparent: true,
+                        opacity: 0.5
                     })
                 );
-                scene.add(grabberPlaceHolder);
-                // grabberPlaceHolder.visible = false;
-                o.grabVR.grabberHookPoint = grabberPlaceHolder;
+                var grabberHook
+                scene.add(grabberHook);
+                grabberHook.visible = false;
+                o.grabVR.grabberHook = grabberHook;
 
+                controllers.push(o);
             },
             grabableObjects: _grabableObjects,
             grabableDistance: _grabableDistance,
             grab: function (c) {
                 var intersects = c.grabVR.raycaster.intersectObjects(_grabableObjects);
                 if (intersects.length > 0) {
-                    c.grabVR.grabberHookPoint.matrix.copy(intersects[0].object.matrixWorld);
-                    c.grabVR.grabberHookPoint.applyMatrix(new THREE.Matrix4().getInverse(c.matrixWorld));
-                    c.add(c.grabVR.grabberHookPoint);
+                    c.grabVR.grabberHook.matrix.copy(intersects[0].object.matrixWorld);
+                    c.grabVR.grabberHook.applyMatrix(new THREE.Matrix4().getInverse(c.matrixWorld));
+                    c.grabVR.mesh.add(c.grabVR.grabberHook);
+
+                    //c.grabVR.mesh.add(c.grabVR.tmpMesh);
+                    //c.grabVR.tmpMesh.position.copy(c.grabVR.grabbedObject.position);
+
+                    //console.dir(c.grabVR.grabberHook.position);
 
                     c.grabVR.grabbedObject = intersects[0].object;
 
                     //console.dir(intersects[0]);
-                    // c.grabVR.grabberPlaceHolder.position.x = intersects[0].point.x;
-                    // c.grabVR.grabberPlaceHolder.position.y = intersects[0].point.y;
-                    // c.grabVR.grabberPlaceHolder.position.z = intersects[0].point.z;
-                    c.grabVR.grabberHookPoint.visible = true;
+                    // c.grabVR.grabberHook.position.x = intersects[0].point.x;
+                    // c.grabVR.grabberHook.position.y = intersects[0].point.y;
+                    // c.grabVR.grabberHook.position.z = intersects[0].point.z;
+                    c.grabVR.grabberHook.visible = true;
                     c.grabVR.hasAGrabbedObject = true;
 
                 }
@@ -96,122 +119,54 @@
             release: function (c) {
                 if (c.grabVR.hasAGrabbedObject) {
                     c.grabVR.hasAGrabbedObject = false;
-                    c.grabVR.releaseObject = true;
-                    //console.dir(c.grabVR.grabbedObject);                    
-                    //c.grabVR.grabbedObject.matrix.copy(c.grabVR.grabbedObject.matrixWorld);
-                    // c.grabVR.grabbedObject.applyMatrix(new THREE.Matrix4().getInverse(scene.matrixWorld));
-                    //scene.add(c.grabVR.grabbedObject);
-                    //c.grabVR.grabbedObject.__dirtyRotation = true;
-                    //c.grabVR.grabbedObject.__dirtyPosition = true;
-                    //c.grabVR.grabbedObject.setAngularVelocity(new THREE.Euler().setFromQuaternion(c.grabVR.lastQuaternion.normalize()));
-                    //c.grabVR.grabbedObject.setLinearVelocity(c.grabVR.velocity.multiplyScalar(100));
-                    //c.grabVR.grabbedObjectHeld = false;
-                    //var tmp = c.grabVR.grabbedObject;
-                    //c.grabVR.grabbedObject = null;
-
-                    // setTimeout(function () {
-                    //     var vec = new THREE.Vector3();
-                    //     c.grabVR.grabberHookPoint.getWorldPosition(vec);
-                    //     c.grabVR.grabbedObject.applyCentralImpulse(c.grabVR.grabbedObject.worldToLocal(vec));
-                    //     //c.grabVR.grabbedObject.__dirtyPosition = true;
-                    //     //c.grabVR.grabbedObject.__dirtyRotation = true;
-                    // }, 10);
-
-                    c.grabVR.grabberHookPoint.visible = false;
+                    c.grabVR.grabberHook.visible = false;
                 }
             },
             update: function (dt) {
-                controllers.forEach(function (controller) {
-                    controller.getWorldPosition(controller.grabVR.raycaster.ray.origin);
+                controllers.forEach(function (c) {
+                    c.getWorldPosition(c.grabVR.raycaster.ray.origin);
+                    c.getWorldQuaternion(c.grabVR.quaternion);
+                    c.grabVR.raycaster.ray.direction.copy(direction).applyEuler(new THREE.Euler().setFromQuaternion(c.grabVR.quaternion, "XYZ"));
 
-                    controller.getWorldQuaternion(controller.grabVR.quaternion);
-                    controller.grabVR.raycaster.ray.direction.copy(direction).applyEuler(new THREE.Euler().setFromQuaternion(controller.grabVR.quaternion, "XYZ"));
-
-
-                    var intersects = controller.grabVR.raycaster.intersectObjects(_grabableObjects);
+                    var intersects = c.grabVR.raycaster.intersectObjects(_grabableObjects);
                     if (intersects.length > 0) {
-                        controller.grabVR.line.visible = true;
+                        c.grabVR.line.visible = true;
                     } else {
-                        controller.grabVR.line.visible = false;
+                        c.grabVR.line.visible = false;
                     }
 
-                    if (controller.grabVR.hasAGrabbedObject) {
-                        // if (controller.grabVR.releaseObject) {
-                        //     controller.grabVR.releaseObject = false;
-                        //     controller.grabVR.hasAGrabbedObject = false;
-                        //     //controller.grabVR.grabbedObject.setLinearVelocity(new THREE.Vector3(0, 0, 0));
-                        //     var vec = new THREE.Vector3();
-                        //     controller.grabVR.grabberHookPoint.getWorldPosition(vec);
-                        //     //controller.grabVR.grabbedObject.applyCentralImpulse(controller.grabVR.grabbedObject.worldToLocal(vec));
-                        //     controller.grabVR.grabbedObject.applyCentralForce(controller.grabVR.grabbedObject.worldToLocal(vec));
-                        // } else {
-                        //     controller.grabVR.grabbedObject.setLinearVelocity(new THREE.Vector3(0, 0, 0));
-                        //     controller.grabVR.grabberHookPoint.getWorldPosition(controller.grabVR.grabbedObject.position);
-
+                    if (c.grabVR.hasAGrabbedObject) {
                         //### for sliding object towards controller
                         var vec = new THREE.Vector3();
-                        controller.getWorldPosition(vec);
-                        controller.grabVR.grabberHookPoint.lookAt(controller.grabVR.mesh.worldToLocal(vec));
-                        //### end for sliding object towards controller
+                        c.getWorldPosition(vec);
+                        c.grabVR.grabberHook.lookAt(c.grabVR.mesh.worldToLocal(vec));
 
+                        //console.dir(c.grabVR.grabberHook);
+                        //### end for sliding object towards controller c
 
                         //     controller.grabVR.grabbedObject.__dirtyPosition = true;
                         //     controller.grabVR.grabbedObject.__dirtyRotation = true;
-                        // }
-                        //controller.grabVR.grabberHookPoint.position.copy(intersects[0].point);
-                        //controller.grabVR.grabbedObject.setLinearVelocity(new THREE.Vector3(0, 0, 0));
+
                         var vec = new THREE.Vector3();
-                        controller.grabVR.grabberHookPoint.getWorldPosition(vec);
-                        vec = controller.grabVR.grabbedObject.worldToLocal(vec).multiplyScalar(4);
-                        vec.applyEuler(controller.grabVR.grabbedObject.rotation);
-                        controller.grabVR.grabbedObject.setLinearVelocity(vec);
-                        //controller.grabVR.grabbedObject.__dirtyPosition = true;
-                        //controller.grabVR.grabbedObject.__dirtyRotation = true;
-                        //controller.grabVR.grabbedObject.setLinearVelocity (controller.grabVR.grabbedObject.worldToLocal(vec).multiplyScalar(3));
+                        c.grabVR.grabberHook.getWorldPosition(vec);
 
-                        //controller.grabVR.grabbedObject.applyImpulse(controller.grabVR.grabbedObject.worldToLocal(vec), controller.grabVR.grabbedObject.getAngularVelocity());
-                        //controller.grabVR.grabbedObject.applyCentralForce(controller.grabVR.grabbedObject.worldToLocal(vec));
-                        //controller.grabVR.grabbedObject.setLinearVelocity(controller.grabVR.grabbedObject.worldToLocal(vec));
-                        //controller.grabVR.grabbedObject.applyTorque  (controller.grabVR.grabbedObject.worldToLocal(vec));
-
-                        //controller.grabVR.grabbedObject.setLinearVelocity();
-                        //'applyCentralImpulse'
+                        //console.dir(vec);
 
 
-
-                        //controller.grabVR.lastPosition.copy(controller.grabVR.grabbedObject.position);
-                        //scene.add(c.grabVR.grabbedObject);
+                        vec = c.grabVR.grabbedObject.worldToLocal(vec).multiplyScalar(4);
+                        vec.applyEuler(c.grabVR.grabbedObject.rotation);
+                        if (_usePhysijs) {
+                            c.grabVR.grabbedObject.setLinearVelocity(vec);
+                        } else { 
+                            c.grabVR.grabberHook.getWorldPosition(c.grabVR.grabbedObject.position);
+                        }
                     }
-                    //if (controller.grabVR.grabbedObject) {
 
-                    //if (controller.grabVR.grabbedObject.position.z < -0.75) {
-                    //if (!controller.grabVR.mesh.grabbedObjectHeld) {
-                    //controller.grabVR.grabbedObject.position.z += 20 * dt;
-                    //}//else{
-                    //controller.grabVR.grabbedObjectHeld = true;
-                    //}
-                    //if (controller.grabVR.grabbedObject.position.z < -0.75) {
-                    //controller.grabVR.grabbedObject.position.z += Math.abs(controller.grabVR.grabbedObject.position.z / 2);
-                    //   controller.grabVR.grabbedObject.applyCentralForce(new THREE.Vector3(0, 0, 1));
-                    //}
-                    // var objectPosition = new THREE.Vector3();
-                    // controller.grabVR.grabbedObject.getWorldPosition(objectPosition);
-                    // controller.grabVR.velocity.subVectors(objectPosition, controller.grabVR.lastPosition);
-                    //controller.grabVR.lastPosition.copy(objectPosition);
-
-                    // var objectQuaternion = new THREE.Quaternion();
-                    // controller.grabVR.grabbedObject.getWorldQuaternion(objectQuaternion);
-                    // controller.grabVR.quaternionDifference.subVectors(objectQuaternion, controller.grabVR.lastQuaternion);
-                    //controller.grabVR.lastQuaternion.copy(objectQuaternion);
-
-                    //}
-
-                    controller.grabVR.mesh.position.copy(controller.grabVR.raycaster.ray.origin);
-                    controller.grabVR.mesh.__dirtyPosition = true;
-                    //controller.grabVR.mesh.quaternion.copy(controller.grabVR.quaternion);
-                    //controller.grabVR.mesh.rotation.copy(controller.rotation);
-                    controller.grabVR.mesh.rotation.copy(new THREE.Euler().setFromQuaternion(controller.grabVR.quaternion, "XYZ"));
-                    controller.grabVR.mesh.__dirtyRotation = true;
+                    c.grabVR.mesh.position.copy(c.grabVR.raycaster.ray.origin);
+                    //c.grabVR.mesh.quaternion.copy(c.grabVR.quaternion);
+                    c.grabVR.mesh.rotation.copy(new THREE.Euler().setFromQuaternion(c.grabVR.quaternion, "XYZ"));
+                    c.grabVR.mesh.__dirtyPosition = true;
+                    c.grabVR.mesh.__dirtyRotation = true;
                 });
             }
         }
